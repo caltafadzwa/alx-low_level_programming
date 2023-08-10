2cc141667da3e8878852b5c38993b7c5726a4b12
caltafadzwa <cgaraba333@gmail.com>
2023-08-10 11:28:12 -0700

    4-puts in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
