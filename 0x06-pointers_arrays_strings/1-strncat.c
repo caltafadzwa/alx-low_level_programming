0b63bb6ebc9334d656c09ede0f8f02a55f39948a
caltafadzwa <cgaraba333@gmail.com>
2023-08-24 07:27:07 -0700

    arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,4 +2,18 @@
 #include <stdio.h>
 
 /**
- *
+ * _strncat - that concatenates two strings
+ * @src: the source of the file
+ * @dest: the destination concerned
+ * Return: void
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char dest[] = "Hello";
+	char src[] = "World!";
+	{
+		printf("Concatenated String: %s\n", strcat(destination, source))
+	}
+	return (0);
+}
